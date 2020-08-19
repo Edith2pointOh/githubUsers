380e4ca3fd12536ad456aab544ddf6ef55c1aa57
Edith Werbel <edith.werbel@gmail.com>
2020-08-18 20:37:43 -0400

    Updates

---
+++ precache-manifest.056c7412504cf863949cc1c456a1ae58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b1ec203d805d788df6bd2eb4b4c45c",
+    "url": "/Edith2pointOh/githubUsers.git/index.html"
+  },
+  {
+    "revision": "9c830906d32fdda05628",
+    "url": "/Edith2pointOh/githubUsers.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3b50ee66406b997b18c",
+    "url": "/Edith2pointOh/githubUsers.git/static/js/2.12591186.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Edith2pointOh/githubUsers.git/static/js/2.12591186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c830906d32fdda05628",
+    "url": "/Edith2pointOh/githubUsers.git/static/js/main.fa948249.chunk.js"
+  },
+  {
+    "revision": "bc436149d2726db4fb50",
+    "url": "/Edith2pointOh/githubUsers.git/static/js/runtime-main.aa52872c.js"
+  }
+]);
